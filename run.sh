b32684568334bb61e5477f8dd41ec87711aeece8
Josh Gruenstein <joshuagruenstein@users.noreply.github.com>
2018-01-10 10:31:07 -0500

    hotfix 0.9.2
    reduce docker inflation

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-docker pull battlecode/battlecode-2018
 
-docker run -it --privileged -p 16147:16147 -p 6147:6147 -v $PWD:/player battlecode/battlecode-2018
+docker stop $(docker ps -q)
+docker container rm $(docker container ls -aq)
+docker volume rm $(docker volume ls -q)$
+docker volume prune
+
+docker run -it --privileged -p 16147:16147 -p 6147:6147 -v $PWD:/player --rm battlecode/battlecode-2018
